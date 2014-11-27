414c3a29712aad9cb7f3e0d9b7820d18852f8ace
Rafael Soutelino <rsoutelino@gmail.com>
2014-11-28 02:46:47 +1300

    fiz o esqueleto do programa proc_ctd.py

---
+++ proc_ctd.py
@@ -0,0 +1,21 @@
+# Script para LER dados de CTD, REMOVER spikes, PLOTAR e SALVAR os dados limpos
+# Autor:  varios
+# Novembro de 2014
+
+
+
+
+def read_ctd():
+    pass 
+
+
+def remove_spike():
+    pass
+
+
+def plot():
+    pass
+
+
+def export_data():
+    pass
