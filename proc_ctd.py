eeea2ca0f0e3533f1d1f8b8fb616f703e56796b9
Rafael Soutelino <rsoutelino@gmail.com>
2014-11-28 03:03:13 +1300

    adicionei opcoes de argumentos de input e output

--- proc_ctd.py
+++ proc_ctd.py
@@ -1,8 +1,17 @@
 # Script para LER dados de CTD, REMOVER spikes, PLOTAR e SALVAR os dados limpos
 # Autor:  varios
 # Novembro de 2014
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--filename', default='ctd_bruto.cnv')
+parser.add_argument('-o', '--output', default='ctd_tratado.txt')
+args = parser.parse_args()
 
+filename = args.filename
+output = args.output
+print "filename = %s" %filename
+print "output = %s" %output
 
 
 def read_ctd():
